82f58b5e9538e43dc49c4a8613a77dec84a089f5
keukenstudiodordrecht <86236896+keukenstudiodordrecht@users.noreply.github.com>
2022-05-11 17:05:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,81 +2,77 @@ const PORT = process.env.PORT || 666;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const {
-    response
-} = require("express");
-const {
-    contains
-} = require("cheerio/lib/static");
+const { response } = require("express");
+const { contains } = require("cheerio/lib/static");
 
 const reviews = [];
 const app = express();
-const reviewPlatforms = [{
-        name: "Klanten Vertellen",
-        adress: "https://www.klantenvertellen.nl/reviews/1035633/keukenstudio_dordrecht_nl"
-    },
-    {
-        name: "Google",
-        adress: "https://www.google.com/search?q=keukenstudio+dordrecht+reviews&oq=keukenstudio+dordrecht+reviews&aqs=chrome..69i57j69i60l2.4572j0j4&sourceid=chrome&ie=UTF-8#lrd=0x47c42f24cd67cfdb:0xd25fbfad59c1ae80,1,,,"
-    },
-    {
-        name: "Facebook",
-        adress: "https://klantenvertellen.nl"
-    },
+const reviewPlatforms = [
+  {
+    name: "Klanten Vertellen",
+    adress:
+      "https://www.klantenvertellen.nl/reviews/1035633/keukenstudio_dordrecht_nl",
+  },
+  {
+    name: "Google",
+    adress:
+      "https://www.google.com/search?q=keukenstudio+dordrecht+reviews&oq=keukenstudio+dordrecht+reviews&aqs=chrome..69i57j69i60l2.4572j0j4&sourceid=chrome&ie=UTF-8#lrd=0x47c42f24cd67cfdb:0xd25fbfad59c1ae80,1,,,",
+  },
+  {
+    name: "Facebook",
+    adress: "https://klantenvertellen.nl",
+  },
 ];
 
-reviewPlatforms.forEach(reviewPlatform => {
-    axios.get(reviewPlatform.adress)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-    })
-})
+reviewPlatforms.forEach((reviewPlatform) => {
+  axios.get(reviewPlatform.adress).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+  });
+});
 
 app.listen(PORT, () => console.log(`Server is running localhost:${PORT}`));
 
-
-
-app.get("/", (req, res) => { // homepage
-    res.json("Welkom op mijn review API");
-    res.set('Access-Control-Allow-Origin', '*');
-    res.sendFile(__dirname + '/message.json');
+app.get("/", (req, res) => {
+  // homepage
+  res.set("Access-Control-Allow-Origin", "*");
+  res.json("Welkom op mijn review API");
 });
 
-app.get("/reviews", (req, res) => { // /reviews
-    res.set('Access-Control-Allow-Origin', '*');
-    res.sendFile(__dirname + '/message.json');
-    axios
-        .get(
-            "https://www.klantenvertellen.nl/reviews/1035633/keukenstudio_dordrecht_nl?lang=nl&limit=100&pageNumber=0&filterRating=0&filterLocale=&filterDateScoreOrder=DATE_DESC"
-        )
-        .then((response) => {
-            const html = response.data;
-            const $ = cheerio.load(html);
+app.get("/reviews", (req, res) => {
+  // /reviews
+  res.set("Access-Control-Allow-Origin", "*");
+  axios
+    .get(
+      "https://www.klantenvertellen.nl/reviews/1035633/keukenstudio_dordrecht_nl?lang=nl&limit=100&pageNumber=0&filterRating=0&filterLocale=&filterDateScoreOrder=DATE_DESC"
+    )
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-            app.get("/reviews/:keyWord", async (req, res) => {
-                console.log(req.params.keyWord);
-            });
+      app.get("/reviews/:keyWord", async (req, res) => {
+        console.log(req.params.keyWord);
+      });
 
-            $('div.review:contains("hoop")', html).each(function () {
-                const title = $("h4", this).text();
-                const reviewText = $("p.text.opinion", this).html();
-                const rating = $("span", this).html();
-                const reaction = $("div.review-response > p", this).text();
-                const source = "KlantenVertellen"
+      $('div.review:contains("hoop")', html).each(function () {
+        const title = $("h4", this).text();
+        const reviewText = $("p.text.opinion", this).html();
+        const rating = $("span", this).html();
+        const reaction = $("div.review-response > p", this).text();
+        const source = "KlantenVertellen";
 
-                reviews.push({
-                    title,
-                    reviewText,
-                    rating,
-                    reaction,
-                    source //: reviewPlatforms.name
-                });
-            });
-
-            res.json(reviews);
-        })
-        .catch(function (err) {
-            console.log(err);
+        reviews.push({
+          title,
+          reviewText,
+          rating,
+          reaction,
+          source, //: reviewPlatforms.name
         });
+      });
+
+      res.json(reviews);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 });
