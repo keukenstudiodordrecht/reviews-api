93faf24e5ed442806f0b8d792a5fcd096fa36306
keukenstudiodordrecht <86236896+keukenstudiodordrecht@users.noreply.github.com>
2022-05-17 17:01:53 +0200

    update

--- review.js
+++ review.js
@@ -3,13 +3,10 @@ keyWord = params.get("keyWord");
 keyWord = document.querySelector("#article > div.lp_content-labels > div:nth-child(3) > span").innerText.toLowerCase();
 const api_url = `https://reviewsksd.herokuapp.com/reviews/${keyWord}`;
 async function getReviews() {
+  console.log(`Loading reviews about ${keyWord}`)
   const response = await fetch(api_url);
   const data = await response.json();
   const d = data[0].reviews["review"];
-  console.log("Raw data: ", data);
-  console.log(data[2]);
-  console.log("Aantal reviews: ", data.length);
-  console.log("Reviews: ", data[0].reviews["review"]);
 
   let tit = "";
   let rev = "";
@@ -32,18 +29,13 @@ async function getReviews() {
 
     ratingToStars(rat);
 
-    console.log(`Rating: ${rat}`);
     rea += data[i].reviews["review"].reaction || "";
-    console.log(`Reactie: ${rea}`);
     nam += data[i].reviews["review"].name || "";
-    console.log(`Reactie: ${nam}`);
     sou += data[i].reviews["review"].source;
-    console.log(`Source ${sou}`);
 
     for (let i = 0; i < parseInt(numberOfStars); i++) {
       ratingStars += `<span class="fa fa-star checked"></span>`;
     }
-    console.log();
 
     function checkNumber(x) {
       // check if the passed value is a number
@@ -53,15 +45,12 @@ async function getReviews() {
           for (let k = 0; k < parseInt(5 % numberOfStars); k++) {
             ratingStars += `<span class="fa fa-star-o"></span>`;
           }
-          console.log(`5 - ratingStars = ${x}. So it's an integer.`);
         } else {
           for (let j = 0; j < 0.5; j++) {
             ratingStars += `<span class="fa fa-star-half-o"></span>`;
           }
-          console.log(`5 - ratingStars = ${x}. So it's a float value.`);
         }
       } else {
-        console.log(`${x} is not a number`);
       }
     }
 
