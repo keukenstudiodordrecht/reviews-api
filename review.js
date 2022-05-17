f95e594af7b24117bf32308da9c0bf2956905263
keukenstudiodordrecht <86236896+keukenstudiodordrecht@users.noreply.github.com>
2022-05-17 16:01:39 +0200

    update

--- review.js
+++ review.js
@@ -73,9 +73,9 @@ async function getReviews() {
         <span id="rev">${rev}</span> <br> <br>
       <span id="rat">Rating: ${rat} ${ratingStars} </span> <br> <br>
       <span id="nam">Geschreven door: ${nam} </span> <br> <br>
-      <span id="rea">Onze reactie: ${rea}</span> <br> <br>
-        <span id="sou"><a href="https://www.klantenvertellen.nl/reviews/1035633/keukenstudio_dordrecht_nl?lang=nl&limit=100&pageNumber=0&filterRating=0&filterLocale=&filterDateScoreOrder=DATE_DESC">${sou}</a></span> <br><br>
-      </p></div>`;
+      // <span id="rea">Onze reactie: ${rea}</span> <br> <br>
+      //   <span id="sou"><a href="https://www.klantenvertellen.nl/reviews/1035633/keukenstudio_dordrecht_nl?lang=nl&limit=100&pageNumber=0&filterRating=0&filterLocale=&filterDateScoreOrder=DATE_DESC">${sou}</a></span> <br><br>
+      // </p></div>`;
 
     document.getElementById("review-card").innerHTML = reviewCard;
 
